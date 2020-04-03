0b151b778cd1e2f6cb42149a7da88046215f2727
gemperline <39207952+gemperline@users.noreply.github.com>
2020-04-03 11:17:36 -0700

    Features  added

---
+++ modal.js
@@ -0,0 +1,58 @@
+/*
+function login() {
+  var email = document.getElementById('usrname').value;
+  var password = document.getElementById('psw').value;
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+    window.alert(errorMessage);
+  });
+
+
+}
+
+function register(){
+
+  var email = document.getElementById('email_register').value;
+  var password = document.getElementById('password_register').value;
+
+  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+      window.alert(errorMessage);
+    });
+}
+*/
+
+function showLoginModal() {
+  document.getElementById('message-modal').style.display='block';
+  document.getElementById('reg-modal').style.display='none';
+}
+function showRegisterModal(){
+  document.getElementById('registerModal').style.display='block';
+  document.getElementById('loginModal').style.display='none';
+}
+
+/*
+// When the user clicks anywhere outside of the modal, close it
+  var modal = document.getElementById('loginModal');
+
+window.onclick = function(event) {
+
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+  var modal = document.getElementById('registerModal');
+window.onclick = function(event) {
+
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}*/
